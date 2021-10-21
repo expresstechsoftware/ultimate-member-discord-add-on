269f2dcaa6839216847d3b3190fc84dab18d5346
younes-dro <dro66_8@hotmail.fr>
2021-10-21 12:46:13 +0100

    Add URL parameter to ets_get_ultimatemember_discord_formated_discord_redirect_url() function

--- includes/functions.php
+++ includes/functions.php
@@ -4,7 +4,7 @@ Common functions
 */
 
 // function to get formated redirect url
-function ets_get_ultimatemember_discord_formated_discord_redirect_url() {
+function ets_get_ultimatemember_discord_formated_discord_redirect_url( $url ) {
 	$parsed = parse_url( $url, PHP_URL_QUERY );
 	if ( $parsed === null ) {
 		return $url .= '?via=discord';
