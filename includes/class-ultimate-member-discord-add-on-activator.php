db0eb9da61e32e32036c748e1b62fb49f142e65e
younes-dro <dro66_8@hotmail.fr>
2021-12-01 06:40:02 +0100

    Update the default option on activation to match the advanced settings fileds

--- includes/class-ultimate-member-discord-add-on-activator.php
+++ includes/class-ultimate-member-discord-add-on-activator.php
@@ -39,21 +39,16 @@ class Ultimate_Member_Discord_Add_On_Activator {
 	 */
 	public static function set_default_setting_values() {
                 
-		update_option( 'ets_ultimatemember_discord_payment_failed', true );
+		
 		update_option( 'ets_ultimatemember_discord_log_api_response', false );
 		update_option( 'ets_ultimatemember_retry_failed_api', true );
 		update_option( 'ets_ultimatemember_discord_job_queue_concurrency', 1 );
 		update_option( 'ets_ultimatemember_discord_job_queue_batch_size', 7 );
 		
 		update_option( 'ets_ultimatemember_retry_api_count', '5' );
-		update_option( 'ets_ultimatemember_send_welcome_dm', true );
+		update_option( 'ets_ultimatemember_discord_send_welcome_dm', true );
 		update_option( 'ets_ultimatemember_discord_welcome_message', 'Hi [ULTIMATEMEMBER_USERNAME] ([ULTIMATEMEMBER_EMAIL]), Welcome, Your membership [ULTIMATEMEMBER_LEVEL] is starting from [ULTIMATEMEMBER_STARTDATE] at [SITE_URL] the last date of your membership is [ULTIMATEMEMBER_ENDDATE] Thanks, Kind Regards, [BLOG_NAME]' );
-		update_option( 'ets_ultimatemember_discord_send_expiration_warning_dm', true );
-		update_option( 'ets_ultimatemember_discord_expiration_warning_message', 'Hi [ULTIMATEMEMBER_USERNAME] ([ULTIMATEMEMBER_EMAIL]), Your membership [ULTIMATEMEMBER_LEVEL] is expiring at [ULTIMATEMEMBER_ENDDATE] at [SITE_URL] Thanks, Kind Regards, [BLOG_NAME]' );
-		update_option( 'ets_ultimatemember_discord_send_membership_expired_dm', true );
-		update_option( 'ets_ultimatemember_discord_expiration_expired_message', 'Hi [ULTIMATEMEMBER_USERNAME] ([ULTIMATEMEMBER_EMAIL]), Your membership [ULTIMATEMEMBER_LEVEL] is expired at [ULTIMATEMEMBER_ENDDATE] at [SITE_URL] Thanks, Kind Regards, [BLOG_NAME]' );
-		update_option( 'ets_ultimatemember_discord_send_membership_cancel_dm', true );
-		update_option( 'ets_ultimatemember_discord_cancel_message', 'Hi [ULTIMATEMEMBER_USERNAME], ([ULTIMATEMEMBER_EMAIL]), Your membership [ULTIMATEMEMBER_LEVEL] at [BLOG_NAME] is cancelled, Regards, [SITE_URL]' );
+		
 	}
 
 }
