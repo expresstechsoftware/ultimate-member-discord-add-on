6d738ae073951590b1242c5fc85f9b0f8aa2edd0
Younes DRO <dro66_8@hotmail.fr>
2022-11-19 01:15:18 +0100

    Update documentation links

--- admin/partials/pages/ulimate_member_discord_documentation.php
+++ admin/partials/pages/ulimate_member_discord_documentation.php
@@ -3,17 +3,17 @@
 		<li>
 			<label><?php esc_html_e( 'YouTube video tutorial on how to connect the plugin with discord server', 'ultimate-member-discord-add-on' ); ?>
 			</label>
-			<a href="#" target="_blank" ><?php esc_html_e( 'Click Here' ); ?> <i class="fa fa-mouse-pointer" aria-hidden="true"></i></a>  
+			<a href="https://www.expresstechsoftwares.com/" target="_blank" ><?php esc_html_e( 'Click Here' ); ?> <i class="fa fa-mouse-pointer" aria-hidden="true"></i></a>  
 		</li>
 		<li>
 			 <label><?php esc_html_e( 'Step-by-Step Written guide on how to connect the plugin with Discord server', 'ultimate-member-discord-add-on' ); ?>
 			</label>
-			<a href="#" target="_blank" ><?php esc_html_e( 'Click Here', 'ultimate-member-discord-add-on' ); ?> <i class="fa fa-mouse-pointer" aria-hidden="true"></i></a>
+			<a href="https://www.expresstechsoftwares.com/" target="_blank" ><?php esc_html_e( 'Click Here', 'ultimate-member-discord-add-on' ); ?> <i class="fa fa-mouse-pointer" aria-hidden="true"></i></a>
 		</li>
 		<li>
 			<label><?php esc_html_e( 'Features and other documentation', 'ultimate-member-discord-add-on' ); ?>
 			</label>
-			<a href="#" target="_blank" ><?php esc_html_e( 'Click Here', 'ultimate-member-discord-add-on' ); ?> <i class="fa fa-mouse-pointer" aria-hidden="true"></i></a>  
+			<a href="https://www.expresstechsoftwares.com/" target="_blank" ><?php esc_html_e( 'Click Here', 'ultimate-member-discord-add-on' ); ?> <i class="fa fa-mouse-pointer" aria-hidden="true"></i></a>  
 		</li>
 	</ul>
 </div>
