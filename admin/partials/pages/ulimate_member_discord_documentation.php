216c510ad4d01dd8de1784b4cbae1248b85b1caa
Younes DRO <dro66_8@hotmail.fr>
2024-01-04 11:52:44 +0100

    Added documentation links for enhanced user guidance.

--- admin/partials/pages/ulimate_member_discord_documentation.php
+++ admin/partials/pages/ulimate_member_discord_documentation.php
@@ -3,20 +3,20 @@
 		<li>
 			<label><?php esc_html_e( 'YouTube video tutorial on how to connect the plugin with discord server', 'ultimate-member-discord-add-on' ); ?>
 			</label>
-			<a href="https://www.expresstechsoftwares.com/" target="_blank" ><?php esc_html_e( 'Click Here' ); ?> <i class="fa fa-mouse-pointer" aria-hidden="true"></i></a>  
+			<a href="https://youtu.be/v7lxB_Bvlv4" target="_blank" ><?php esc_html_e( 'Click Here' ); ?> <i class="fa fa-mouse-pointer" aria-hidden="true"></i></a>  
 		</li>
 		<li>
 			 <label><?php esc_html_e( 'Step-by-Step Written guide on how to connect the plugin with Discord server', 'ultimate-member-discord-add-on' ); ?>
 			</label>
-			<a href="https://www.expresstechsoftwares.com/" target="_blank" ><?php esc_html_e( 'Click Here', 'ultimate-member-discord-add-on' ); ?> <i class="fa fa-mouse-pointer" aria-hidden="true"></i></a>
+			<a href="https://www.expresstechsoftwares.com/ultimate-member-discord-addon/" target="_blank" ><?php esc_html_e( 'Click Here', 'ultimate-member-discord-add-on' ); ?> <i class="fa fa-mouse-pointer" aria-hidden="true"></i></a>
 		</li>
 		<li>
 			<label><?php esc_html_e( 'Features and other documentation', 'ultimate-member-discord-add-on' ); ?>
 			</label>
-			<a href="https://www.expresstechsoftwares.com/" target="_blank" ><?php esc_html_e( 'Click Here', 'ultimate-member-discord-add-on' ); ?> <i class="fa fa-mouse-pointer" aria-hidden="true"></i></a>  
+			<a href="https://www.expresstechsoftwares.com/discord-add-ons-documentation/" target="_blank" ><?php esc_html_e( 'Click Here', 'ultimate-member-discord-add-on' ); ?> <i class="fa fa-mouse-pointer" aria-hidden="true"></i></a>  
 		</li>
 		<li>
-			<b>Opt-out : </b><label><?php esc_html_e( 'By checking the box "Data erases on uninstall?" in advanced settings Tab , you are indicating that you want to delete all data associated with the plugin when it is uninstalled. This includes any settings, preferences, or other data that the plugin may have saved on your system. Please note that this action cannot be undone, so be sure that you want to delete the data before uninstalling the plugin.', 'ultimate-member-discord-add-on' ); ?></label>
+			<b><?php esc_html_e( 'Opt-out :', 'ultimate-member-discord-add-on' ); ?> </b><label><?php esc_html_e( 'By checking the box "Data erases on uninstall?" in advanced settings Tab , you are indicating that you want to delete all data associated with the plugin when it is uninstalled. This includes any settings, preferences, or other data that the plugin may have saved on your system. Please note that this action cannot be undone, so be sure that you want to delete the data before uninstalling the plugin.', 'ultimate-member-discord-add-on' ); ?></label>
 		</li>
 	</ul>
 </div>
